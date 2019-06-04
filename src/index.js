49b40ea61ec8f5be37b3e2610e8e1fbe229593de
Judy Huynh <jhuynh@mossbeachhomes.com>
2019-06-04 16:43:55 -0700

    initializing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+//Import React and ReactDOm libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const buttonText='Click me!!!'
+//Create React component
+const App = () => {
+  return (
+    <div>
+    <label className="label" htmlFor="name">
+    Enter Name:
+    </label>
+    <input id="name" type="text" />
+    <button style={{backgroundColor:'blue', color: 'white'}}>
+      {buttonText}
+    </button>
+   </div>
+ );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Take react component and show it on screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
